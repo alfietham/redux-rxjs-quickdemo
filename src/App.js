14820c5467734db07728eb066e2d8651ad5833a4
Alfie Tham <atham@tribalscale.com>
2019-04-25 16:09:19 -0400

    refactor: App.js to use styled-components + move store to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import "./App.css";
+import { Provider } from "react-redux";
+import styled from "styled-components";
+
+import store from "./store";
+import NavBar from "./components/NavBar";
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 20vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">Furry Friends</header>
-  </div>
+  <Provider store={store}>
+    <Header>Furry Friends</Header>
+    <NavBar />
+  </Provider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
-import store from "./store";
 import "./index.css";
 import App from "./App";
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
