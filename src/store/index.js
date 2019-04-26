179a477767ce07f5264eb0285d4093be0f440607
Alfie Tham <atham@tribalscale.com>
2019-04-26 15:24:12 -0400

    chore: setup thunk middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,23 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./rootReducer";
 
-const reduxDevtools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// Redux devtool
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
-const store = createStore(rootReducer, reduxDevtools);
+// Thunk middleware
+const thunk = store => next => action =>
+  typeof action === "function"
+    ? action(store.dispatch, store.getState)
+    : next(action);
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
